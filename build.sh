d9d338475c8947b65be57ee308acb374f04c39a3
Jason Ivey <jason.ivey@movenetworks.com>
2016-07-22 17:02:51 -0600

    adding build scripts

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# vim:softtabstop=4:ts=4:sw=4:expandtab:tw=120
+
+set -e
+
+if [ "$(uname)" == "Darwin" ]; then
+    COMPILER=clang 
+elif [ "$(uname -s)" == "Linux" ]; then
+    COMPILER=clang-3.8
+fi
+
+echo
+echo "Building/Testing libqmx clang-3.8 debug"
+./waf configure --clang=$COMPILER "$@"
+./waf build install check "$@"
+
+echo
+echo "Successfully built and tested libqmx"
+
