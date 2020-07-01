0d5a5a4cd037cb29c59ce5c692237c792bf755da
Jason Ivey <jasonivey@gmail.com>
2020-07-01 00:08:09 -0600

    Add/Fix: add the softwareengineering.txt fortunes along with installation script. Fix update_alternatives so it uses the default installation directory chosen by the vim build config script.

--- update_alternatives_vim.sh
+++ update_alternatives_vim.sh
@@ -3,22 +3,22 @@
 
 set -e
 
-echo -e "\e[31mCreating links to the existing vim binaries\e[0m"
+echo -e "\e[32mCreating links to the existing vim binaries\e[0m"
 
-sudo update-alternatives --install /usr/bin/editor   editor   /opt/vim/bin/vim      1500
-sudo update-alternatives --install /usr/bin/eview    eview    /opt/vim/bin/eview    1500
-sudo update-alternatives --install /usr/bin/evim     evim     /opt/vim/bin/evim     1500
-sudo update-alternatives --install /usr/bin/ex       ex       /opt/vim/bin/ex       1500
-sudo update-alternatives --install /usr/bin/gview    gview    /opt/vim/bin/gview    1500
-sudo update-alternatives --install /usr/bin/gvim     gvim     /opt/vim/bin/gvim     1500
-sudo update-alternatives --install /usr/bin/gvimdiff gvimdiff /opt/vim/bin/gvimdiff 1500
-sudo update-alternatives --install /usr/bin/rgview   rgview   /opt/vim/bin/rgview   1500
-sudo update-alternatives --install /usr/bin/rgvim    rgvim    /opt/vim/bin/rgvim    1500
-sudo update-alternatives --install /usr/bin/rview    rview    /opt/vim/bin/rview    1500
-sudo update-alternatives --install /usr/bin/rvim     rvim     /opt/vim/bin/rvim     1500
-sudo update-alternatives --install /usr/bin/vi       vi       /opt/vim/bin/vim      1500
-sudo update-alternatives --install /usr/bin/view     view     /opt/vim/bin/view     1500
-sudo update-alternatives --install /usr/bin/vim      vim      /opt/vim/bin/vim      1500
-sudo update-alternatives --install /usr/bin/vimdiff  vimdiff  /opt/vim/bin/vimdiff  1500
+[ -e "/usr/local/bin/vim" ]      && sudo update-alternatives --install /usr/bin/editor   editor   /usr/local/bin/vim      1500
+[ -e "/usr/local/bin/eview" ]    && sudo update-alternatives --install /usr/bin/eview    eview    /usr/local/bin/eview    1500
+[ -e "/usr/local/bin/evim" ]     && sudo update-alternatives --install /usr/bin/evim     evim     /usr/local/bin/evim     1500
+[ -e "/usr/local/bin/ex" ]       && sudo update-alternatives --install /usr/bin/ex       ex       /usr/local/bin/ex       1500
+[ -e "/usr/local/bin/gview" ]    && sudo update-alternatives --install /usr/bin/gview    gview    /usr/local/bin/gview    1500
+[ -e "/usr/local/bin/gvim" ]     && sudo update-alternatives --install /usr/bin/gvim     gvim     /usr/local/bin/gvim     1500
+[ -e "/usr/local/bin/gvimdiff" ] && sudo update-alternatives --install /usr/bin/gvimdiff gvimdiff /usr/local/bin/gvimdiff 1500
+[ -e "/usr/local/bin/rgiew" ]    && sudo update-alternatives --install /usr/bin/rgview   rgview   /usr/local/bin/rgview   1500
+[ -e "/usr/local/bin/rgvim" ]    && sudo update-alternatives --install /usr/bin/rgvim    rgvim    /usr/local/bin/rgvim    1500
+[ -e "/usr/local/bin/rview" ]    && sudo update-alternatives --install /usr/bin/rview    rview    /usr/local/bin/rview    1500
+[ -e "/usr/local/bin/rvim" ]     && sudo update-alternatives --install /usr/bin/rvim     rvim     /usr/local/bin/rvim     1500
+[ -e "/usr/local/bin/vi" ]       && sudo update-alternatives --install /usr/bin/vi       vi       /usr/local/bin/vim      1500
+[ -e "/usr/local/bin/view" ]     && sudo update-alternatives --install /usr/bin/view     view     /usr/local/bin/view     1500
+[ -e "/usr/local/bin/vim" ]      && sudo update-alternatives --install /usr/bin/vim      vim      /usr/local/bin/vim      1500
+[ -e "/usr/local/bin/vimdiff" ]  && sudo update-alternatives --install /usr/bin/vimdiff  vimdiff  /usr/local/bin/vimdiff  1500
 
-echo -e "\e[31mCreated all links to the existing vim binaries\e[0m"
+echo -e "\e[32mCreated all links to the existing vim binaries\e[0m"
