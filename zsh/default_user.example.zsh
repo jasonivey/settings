a45afaf5999278358c80ccad386e65060bdd80b6
Jason Ivey <jasonivey@gmail.com>
2020-12-15 13:12:05 -0700

    Fix: comment inside default_user.example.zsh.  Add: config files for autopep8 (pycodestyle) and flake8

--- zsh/default_user.example.zsh
+++ zsh/default_user.example.zsh
@@ -1,7 +1,6 @@
 #!/usr/bin/env zsh
 # vim: awa:sts=4:ts=4:sw=4:et:cin:fdm=manual:tw=120:ft=zsh
 
-# Since each profile may have a different name take this example file, copy it to "user-default.zsh"
-#  and create the symbolic link in the $HOME directory.
+# Since each profile may have a different name take this example file, copy it to "default_user.zsh"
 export DEFAULT_USER="jasoni"
 
