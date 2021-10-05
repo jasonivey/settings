e5b3da5ec2baeaa9630ee1725abb1ed73fade3ef
Jason Ivey <jasonivey@gmail.com>
2021-10-04 23:35:10 -0600

    Fixing the fzf configuration so it is platform agnostic

--- config/.config/fzf/fzf.zsh
+++ config/.config/fzf/fzf.zsh
@@ -5,14 +5,28 @@ export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if ! command -v fzf &> /dev/null; then
+    if [[ -d /usr/local/opt/fzf/bin ]]; then
+        export PATH="${PATH}:/usr/local/opt/fzf/bin"
+    fi
 fi
+# if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+#  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+# fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+if [[ -e "/usr/local/opt/fzf/shell/completion.zsh" ]]; then
+    source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+elif [[ -e "/usr/share/fzf/completion.zsh" ]]; then
+    source "/usr/share/fzf/completion.zsh" 2> /dev/null
+fi
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+# source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+if [[ -e "/usr/local/opt/fzf/shell/key-bindings.zsh" ]]; then
+    source "/usr/local/opt/fzf/shell/key-bindings.zsh" 2> /dev/null
+elif [[ -e "/usr/share/fzf/key-bindings.zsh" ]]; then
+    source "/usr/share/fzf/key-bindings.zsh" 2> /dev/null
+fi
